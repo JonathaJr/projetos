c3c88783f9e7616122839470fc9b0e3b5b3de99e
Jonathan Junior <jonathanjroliveira07@gmail.com>
2024-08-11 08:00:34 -0300

    devclubRefri

--- devClub/animação_latinha/js/script.js
+++ devClub/animação_latinha/js/script.js
@@ -1,1 +1,24 @@
- 
+let list = document.querySelectorAll('.item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+
+let contagem = list.length; // Retorna 3, pois há 3 item na lista 
+let active = 0; // Esse é o item que está "active" no HTML
+
+next.onclick = () => {
+    let activeOld = document.querySelector('.active');
+    activeOld.classList.remove('active');
+
+    active =  active >= contagem -1; ? 0 : active++; 
+    list[active].classList.add('active');
+}
+
+prev.onclick = () => {
+    let activeOld = document.querySelector('.active');
+    activeOld.classList.remove('active');
+}
+
+
+
+

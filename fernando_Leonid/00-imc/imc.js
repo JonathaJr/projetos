f1b9c338318f7fa037cd260df1e44aa1e5b7f5e4
Jonathan Junior <jonathanjroliveira07@gmail.com>
2024-10-19 17:03:49 -0300

    imc

--- fernando_Leonid/00-imc/imc.js
+++ fernando_Leonid/00-imc/imc.js
@@ -1,35 +1,33 @@
-const calcular = document.getElementById('calcular');
+
+const calcular = document.querySelector("#calcular");
+
+
 
 function imc () {
-    const nome = document.getElementById('nome').value;
-    const altura = +document.getElementById('altura').value;
-    const peso = +document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
+    
+    const nome = document.querySelector("#nome").value;
+    const altura = document.querySelector("#altura").value;
+    const peso = document.querySelector("#peso").value;
+    const resultado = document.querySelector("#resultado");
 
     if (nome !== '' && altura !== '' && peso !== '') {
-
+        
         const valorIMC = (peso / (altura * altura)).toFixed(1);
 
         let classificacao = '';
 
-        if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
-        }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
-        }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
-        }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
-        }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+        if (valorIMC < 18.5) {
+            classificacao = 'abaixo do peso'
+        }else if (valorIMC < 25){
+            classificacao = 'Está com peso ideal. PARABENS!!'
+        }else if (valorIMC < 30) {
+            classificacao = 'Acima do peso.'
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-        
-    }else {
-        resultado.textContent = 'Preencha todos os campos!!!';
+        resultado.textContent = `Prazer ${(nome.toUpperCase())} o seu IMC é ${valorIMC} e você esta ${classificacao}`
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos!!!'
     }
 
 }
